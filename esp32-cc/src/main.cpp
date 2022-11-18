762079782984fc128ba2e39a81b3055c4778bd57
wa1tnr <wa1tnr@users.noreply.github.com>
2022-11-18 14:30:01 +0000

    several - see comment this commit
      include testbed
      define crlf macros
      some npx support not working yet
      funky baud rate 74880 no help
      color wheel referenced
    
    	modified:   main.cpp
    
    On branch dvlp-aa

--- esp32-cc/src/main.cpp
+++ esp32-cc/src/main.cpp
@@ -1,7 +1,19 @@
-// Fri 18 Nov 01:52:29 UTC 2022
+// Fri 18 Nov 11:37:36 UTC 2022
 // +cpl +println
 
+#define NOWDATE "Fri 18 Nov 11:37:36 UTC 2022"
+
 #include <Arduino.h>
+#include <Adafruit_TestBed.h>
+
+extern Adafruit_TestBed TB;
+
+#define cr _cr(); // carriage return
+#define lf _lf(); // carriage return
+#define crlf _cr();_lf();
+
+void _cr() { Serial.write(0x0d); }
+void _lf() { Serial.write(0x0a); }
 
 void cpl() {
     int pin = LED_BUILTIN ;
@@ -10,9 +22,16 @@ void cpl() {
     digitalWrite(pin, state);
 }
 
+#define NEOPIXEL_PIN 0
+
+void setup_rgb() {
+    pinMode(NEOPIXEL_I2C_POWER, OUTPUT);
+    digitalWrite(NEOPIXEL_I2C_POWER, HIGH);
+}
+
 void setup_gpio() {
     pinMode(LED_BUILTIN, OUTPUT);
-    // cpl();
+    setup_rgb();
 }
 
 void clrSerial() {
@@ -22,9 +41,17 @@ void clrSerial() {
     }
 }
 
+
+void signon_msg() {
+    // https://docs.platformio.org/en/stable/projectconf/section_env_build.html
+    // Serial.println("MYSTRING=<%s>\n", MYSTRING);
+    Serial.println(MYSTRING);
+}
+
 void setup_serial() {
     if (!Serial) {
-        Serial.begin(115200);
+        // Serial.begin(115200);
+        Serial.begin(74880);
         clrSerial();
     }
 
@@ -47,18 +74,27 @@ void setup_serial() {
 #endif
 
     delay(700);
+    crlf
+    crlf
+    crlf
+    crlf
     Serial.println("begin program.");
 }
 
 void setup(void) {
     delay(700);
     setup_serial();
-    setup_gpio();
-    delay(700);
+    setup_gpio(); delay(700);
+    crlf signon_msg(); delay(700);
 }
 
+uint8_t wheelColor=0;
+
 void loop(void) {
-    Serial.println("  I am looping: cdefg 02:39:30z  ");
+    Serial.print("  I am looping: efghi ");
+    // Serial.println(NOWDATE);
+    // Serial.println(MYSTRING);
+    Serial.println(MYOTHERSTR);
     cpl();
     delay(1000);
     // while(-1);
