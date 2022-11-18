d6d0879df3212f660f04e0d9d06cd2be5082fa50
wa1tnr <wa1tnr@users.noreply.github.com>
2022-11-18 01:15:39 +0000

    adds cpl pinmode serial
    	modified:   main.cpp
    
    On branch dvlp-aa

--- esp32-cc/src/main.cpp
+++ esp32-cc/src/main.cpp
@@ -1,14 +1,38 @@
+// Fri 18 Nov 01:14:05 UTC 2022
+// +cpl +println
+
 #include <Arduino.h>
 
-void setup(void) {
-    delay(700);
-    Serial.begin(9600);
+void cpl() {
+    int pin = LED_BUILTIN ;
+    bool state = digitalRead(pin);
+    state = !state;
+    digitalWrite(pin, state);
+}
+
+void setup_gpio() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    cpl();
+}
+
+void setup_serial() {
+    Serial.begin(115200);
     while(!Serial);
+    delay(700);
     Serial.println("begin program.");
 }
 
+void setup(void) {
+    delay(700);
+    setup_gpio();
+    setup_serial();
+}
+
 void loop(void) {
-    while(-1);
+    Serial.println("I am looping: bcdef ");
+    cpl();
+    delay(1000);
+    // while(-1);
 }
 
 // END.
