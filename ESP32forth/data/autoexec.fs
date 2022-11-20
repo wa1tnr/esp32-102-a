c04ae2fb67f413f5dc1cc6ab5816c73b421509b7
wa1tnr <wa1tnr@users.noreply.github.com>
2022-11-20 21:47:13 +0000

    forth source file auto execute
      optional - not required
    
    	new file:   autoexec.fs
    
    On branch dvlp-aa

---
+++ ESP32forth/data/autoexec.fs
@@ -0,0 +1,8 @@
+\ comment
+
+: fvlb space space ." flava bean " ;
+: aexc cr cr ." This is your spiff - try 'make spiffs' " cr cr
+  ." Sun 20 Nov 21:39:44 UTC 2022  autoexec.fs   kelto" fvlb ;
+aexc cr
+
+\ end.
