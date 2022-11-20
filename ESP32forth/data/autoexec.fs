89d394dfdc86c02153ac3c3d7c3ef26e8dacf7a1
wa1tnr <wa1tnr@users.noreply.github.com>
2022-11-20 22:04:55 +0000

    update spiff

--- ESP32forth/data/autoexec.fs
+++ ESP32forth/data/autoexec.fs
@@ -1,8 +1,9 @@
 \ comment
 
 : fvlb space space ." flava bean " ;
-: aexc cr cr ." This is your spiff - try 'make spiffs' " cr cr
-  ." Sun 20 Nov 21:39:44 UTC 2022  autoexec.fs   kelto" fvlb ;
-aexc cr
+: aexc cr ." This is your spiff - try 'make spiffs' " cr cr
+  ." Sun 20 Nov 22:04:41 UTC 2022   data/autoexec.fs   kelto" fvlb ;
+
+-98 -97 aexc
 
 \ end.
