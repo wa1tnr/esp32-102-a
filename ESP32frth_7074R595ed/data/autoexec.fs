730d88e54a9f7ae6ff9e0ea15e54ff72b68b3b86
wa1tnr <wa1tnr@users.noreply.github.com>
2022-11-21 21:00:57 +0000

    omission - modified autoexec to suit inclusion
    	modified:   data/autoexec.fs
    
    On branch dvlp-aa

--- ESP32frth_7074R595ed/data/autoexec.fs
+++ ESP32frth_7074R595ed/data/autoexec.fs
@@ -1,15 +1,26 @@
-\ ." Mon 21 Nov 16:06:31 UTC 2022"
+\ ." Mon 21 Nov 16:17:16 UTC 2022"
+
+include bobby.fs
+include cubby.fs
+include roy.fs
+
+: delay 500 * 0 do 1 drop loop ;
+
+: bcr space space bobby 2000 delay
+  cubby 2000 delay
+  roy space space cr ;
 
 : fvlb space space ." flava bean" ;
-: aexts ." Mon 21 Nov 16:06:31 UTC 2022" ;
+: aexts ." Mon 21 Nov 20:27:21 UTC 2022" ;
 : sayw ."  SAY:    'words'  or  'bye'" ;
 : aexc cr ." This is your spiff - try 'make spiffs' at linux shell (or equivalent) " cr cr
   aexts
-  ."   data/autoexec.fs   mst3k" fvlb cr cr sayw cr cr ;
+  ."   data/autoexec.fs   mst3k" fvlb cr cr sayw cr cr
+  bcr cr cr
+;
 
 \ latest version of the program prints on the following line in the terminal (not here).
 
-: delay 500 * 0 do 1 drop loop ;
 : LEDv2 13 ;
 : ledSetup LEDv2 output pinmode ;
 : blink high LEDv2 pin 44 delay low  LEDv2 pin 888 delay ;
