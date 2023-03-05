97a2f2ad44ec93f9a72b4896ff4d9bb838a8b411
wa1tnr <wa1tnr@users.noreply.github.com>
2023-03-05 18:45:31 +0000

    plus stack effect diagrams

--- ESP32wireless-b/data/data_dumper-a.fs
+++ ESP32wireless-b/data/data_dumper-a.fs
@@ -1,42 +1,24 @@
-
- ( top of sub-program   OPERATIONAL 00:56z Sunday 5 March 2023 )
-
-hex 500C2C00
-hex 5 3 * .
-
+ ( top of sub-program 18:26z 2023 05 Mar Sunday )
+hex 500C2C00 ( -- addr )
+hex 5 3 * . ( -- )
 : stripval ( n -- n<128 )
-  dup 7f and
-;
-
+  dup 7f and ;
 : nexchEmit ( addr -- addr+1 )
   dup c@ dup dup stripval = if
       dup 1F > if
-          drop emit
-          1 + exit
+          drop emit 1 + exit
       then
   then
-  drop drop [char] . emit
-  1 + \ plus
-;
-
-\ optional macros -----
-
-: nc ( -- ) nexchEmit ; \ alias
-: row 8 dup + 0 do nc loop
-;
-: frow 4 0 do row loop ;
-: stanz 4 0 do frow cr loop ;
-: group 4 0 do stanz  loop ;
-
-: bfscan 8 dup * 0 do group loop ;
-
-: goferit  swap drop dup bfscan ;
-
-: kurtz 3F803400 700 +  group space group space  ." pirhana sp"  ;
-
-here 2000 - \ minus
-
+  drop drop [char] . emit 1 + ;
+: nc ( addr -- addr+1 ) nexchEmit ; \ alias  ----- optional macros -----
+: row ( addr -- addr+8 ) 8 dup + 0 do nc loop ;
+: frow ( addr -- addr+32 ) 4 0 do row loop ;
+: stanz ( addr -- addr+128 ) 4 0 do frow cr loop ;
+: group ( addr -- addr+512 ) 4 0 do stanz  loop ;
+: bfscan ( addr -- addr+32768 ) 8 dup * 0 do group loop ; \ 64 x 512
+: goferit ( addr addr' -- addr'+32768 ) swap drop dup bfscan ;
+: kurtz ( addr -- addr+1024 ) 3F803400 700 +  group space group space  ." octopii gmbh "  ;
+: giveagoodhoot  40C00000 50000000 ffff - ffff - ffff - ffff -   ffff - ffff - ffff - ffff - ;
+here 2000 - ( -- addr-0x2000 )
+: version ( -- ) ." 05 Mar 2023   0.0.0bb-" cr ;
 ( end )
-
-: version ." 0.0.0aa-" cr ;
-\ END.
