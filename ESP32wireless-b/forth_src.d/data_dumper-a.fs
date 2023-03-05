e867d98f65118eafe940bdb8b3465a7356e61c6d
wa1tnr <wa1tnr@users.noreply.github.com>
2023-03-05 00:55:05 +0000

    one-shot commented file will delete comments
       save this if you want it as it's going away.
    
       don't want the comments lingering. ;)
    
    	modified:   data_dumper-a.fs
    
    On branch dvlp-aa

--- ESP32wireless-b/forth_src.d/data_dumper-a.fs
+++ ESP32wireless-b/forth_src.d/data_dumper-a.fs
@@ -7,14 +7,18 @@ hex 5 3 * .
   dup 7f and
 ;
 
-: nexchEmit ( n -- n+1 )
-  dup c@ dup dup stripval = if
-      dup 1F > if
-          drop emit 1 + exit
+: nexchEmit ( addr -- addr+1 )
+  dup c@ dup dup stripval = if \ if it didn't need stripping, after all..
+      dup 1F > if   \ only printable chars survive
+          drop emit \ print to console
+          1 + exit  \ promised address increment
       then
   then
-  drop drop [char] . emit 1 + \ plus
+  drop drop [char] . emit \ non-printable char
+  1 + \ increment addr
 ;
+
+\ everything below this line is an optional 'macro'
 
 : nc ( -- ) nexchEmit ; \ alias
 : row 8 dup + 0 do nc loop
@@ -32,3 +36,11 @@ hex 5 3 * .
 here 2000 - \ minus
 
 ( end )
+
+\ Considering making the second one also a mask op:
+
+\ dup 7f and
+
+\ dup 1F > if
+
+\ END.
