c5d9fbcdc7fffe89003ad7496f898d089c213035
wa1tnr <wa1tnr@users.noreply.github.com>
2024-03-25 00:00:13 +0000

    likely to be in sync w production
    	modified:   recent.fs
    
    On branch dvlp-aa

--- vsc-wokwi-esp32Forth-a/data/recent.fs
+++ vsc-wokwi-esp32Forth-a/data/recent.fs
@@ -1,13 +1,11 @@
 \ recent.fs
 \ 24 March 2024
 
-\ waitfor.fs  24 March 20:45z
 decimal
 VARIABLE iterator
 : lwaitfor 
   key? IF QUIT THEN
-  iterator @ 1 + dup . iterator !
-  cr .s cr
+  iterator @ 1 + iterator !
   depth 0= IF QUIT THEN
   depth 10 - 0= IF ." ten minus" cr QUIT THEN
 ; 
@@ -18,12 +16,11 @@ hex
 : scan g blink dup . ;
 : scans 0 do scan loop ;
 
-: bfscans ( -- )
+: bfscans ( n -- )
   0 do
       bfscan
-      blink blink cr dup . cr
-      lwaitfor
-      8000 ms
+      blink blink
+      200 0 do lwaitfor loop
   loop
 ;
 
